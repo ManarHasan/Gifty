b8e866268847e8dbe2e92e4ddd4c0f608b5fe829
manarHasan <manar.hasan62@gmail.com>
2021-01-19 11:38:06 +0200

    models Manar

---
+++ Gifty/gifty/src/main/java/com/axsos/gifty/models/Cart.java
@@ -0,0 +1,73 @@
+package com.axsos.gifty.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cart")
+public class Cart {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private int quantity;
+	@Column(updatable=false)
+	private Date createdAt;
+	private Date updatedAt;
+	
+	public Cart () {}
+	
+	public Cart(int quantity) {
+		super();
+		this.quantity = quantity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+
+}
---
+++ Gifty/gifty/src/main/java/com/axsos/gifty/models/User.java
@@ -0,0 +1,5 @@
+package com.axsos.gifty.models;
+
+public class User {
+
+}
