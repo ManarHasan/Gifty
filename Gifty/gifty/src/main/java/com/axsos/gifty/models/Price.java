c7c59ee6dd38a88f840f5865854b2f40c07c2881
manarHasan <manar.hasan62@gmail.com>
2021-01-19 14:18:10 +0200

    Fix models

--- Gifty/gifty/src/main/java/com/axsos/gifty/models/Price.java
+++ Gifty/gifty/src/main/java/com/axsos/gifty/models/Price.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
